43863da96d6db956c997705714d9fcfb4b9e7c58
Paradox-77 <66990101+Paradox-77@users.noreply.github.com>
2020-12-09 11:39:56 +0530

    Added bot file structure and server online features

---
+++ index.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+
+
+client.once('ready', () => {
+    console.log('Discord-Bot is initialized and online.');
+});
+
+
+
+client.login('Nzg1MDkyMzM1Njc4NjUyNDc4.X8y0Nw.d_0DLtJ6oIFyGKCTHq1s-eiCTjY');
