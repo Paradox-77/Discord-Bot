8e7f8b2a034f423a434fa6c34a82a9fc5d754439
Paradox-77 <66990101+Paradox-77@users.noreply.github.com>
2020-12-10 18:52:28 +0530

    Added first static command (-ping)

--- index.js
+++ index.js
@@ -2,12 +2,21 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
-
+const prefix = '-'
 
 client.once('ready', () => {
     console.log('Discord-Bot is initialized and online.');
 });
 
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
 
+    const args = message.content.slice(prefix.length).split(/ +/)
+    const command = args.shift().toLowerCase();
 
-client.login('Nzg1MDkyMzM1Njc4NjUyNDc4.X8y0Nw.d_0DLtJ6oIFyGKCTHq1s-eiCTjY');
+    if(command === 'ping'){
+        message.channel.send('pong!');
+    }
+});
+
+client.login('NzQyMjA4ODQ2MDI1NDU3NzE0.XzCx2g.qKMhbexnOO8u1G1dlv-816bjkKM');
