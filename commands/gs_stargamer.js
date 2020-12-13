9ce4e0a3b6fd00452faed8897d5eff28df80114a
Paradox 77 <adityaa@kaevee.com>
2020-12-13 06:45:16 +0000

    Deleted GS_StarGamer Command File

--- commands/gs_stargamer.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'gs_stargamer',
-    description: "Gives description about GS_StarGamer.",
-    execute(message, args){
-        message.channel.send('Some guy who plays video games, makes YT vids and doesnt know how to code.\nDid i also mention he annoys people alot. And also he has the longest description.');
-    }
-}
