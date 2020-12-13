4496d08dbdd812603ac2e6a75e510a3e672669d6
Paradox 77 <adityaa@kaevee.com>
2020-12-13 06:58:13 +0000

    Changed Embed

--- commands/test_embed.js
+++ commands/test_embed.js
@@ -6,18 +6,11 @@ module.exports = {
         .setColor('#8600ff')
         .setTitle('Test')
         .setURL('https://paradoxic.ml')
-        .setAuthor('Paradox_77')
         .setDescription('This is a test')
-        .setThumbnail('https://paradoxic.ml/logoParadox.png')
         .addFields(
             { name: 'Test ID 1', value: 'Test ID 2'}
-
-
-
         )
-        .setImage('https://paradoxic.ml/logoParadox.png')
-        .setTimestamp()
-        .setFooter('Test ID 3', 'https://paradoxic.ml/logoParadox.png');
+        .setFooter('Help', 'https://paradoxic.ml/logoParadox.png');
 
         message.channel.send(newEmbed);
     }
